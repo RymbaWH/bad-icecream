c74a74138d5ddd9c828b854e3af1c5a303824c41
Klimenty <klim.shchegolkov@mail.ru>
2022-01-28 00:12:10 +0300

    Готовый вариант игры (без финишного окна, таймера, выведения рекорда по времени и кнопри возрата к меню при проигрыше/выйгрыше)

--- main.py
+++ main.py
@@ -22,6 +22,19 @@ def load_image(name, colorkey=None):
     return image
 
 
+def snowflakes(snow_list):
+    for i in range(len(snow_list)):
+        pygame.draw.circle(screen, (110, 173, 245), snow_list[i], 3)
+
+        snow_list[i][1] += 1
+
+        if snow_list[i][1] > 550:
+            y = random.randrange(-50, -10)
+            snow_list[i][1] = y
+            x = random.randrange(0, 550)
+            snow_list[i][0] = x
+
+
 def terminate():
     pygame.quit()
     sys.exit()
@@ -52,21 +65,10 @@ def start_screen():
 
         screen.fill((255, 255, 255))
         screen.blit(fon, (20, 0))
+
+        snowflakes(snow_list)
+
         all_sprites.draw(screen)
-
-        for i in range(len(snow_list)):
-
-            # нарисовать снежинку
-            pygame.draw.circle(screen, (110, 173, 245), snow_list[i], 3)
-
-            # снежинка вниз на 1
-            snow_list[i][1] += 1
-
-            if snow_list[i][1] > 550:
-                y = random.randrange(-50, -10)
-                snow_list[i][1] = y
-                x = random.randrange(0, 550)
-                snow_list[i][0] = x
 
         pygame.display.flip()
         clock.tick(80)
@@ -99,25 +101,12 @@ def third_screen():
                     return '2'
 
                 elif 505 < event.pos[0] < 535 and 75 < event.pos[1] < 100:
-                    print(1)
+                    terminate()
 
         screen.fill((255, 255, 255))
+        snowflakes(snow_list)
         screen.blit(fon, (30, 70))
         all_sprites.draw(screen)
-
-        for i in range(len(snow_list)):
-
-            # нарисовать снежинку
-            pygame.draw.circle(screen, (110, 173, 245), snow_list[i], 3)
-
-            # снежинка вниз на 1
-            snow_list[i][1] += 1
-
-            if snow_list[i][1] > 550:
-                y = random.randrange(-50, -10)
-                snow_list[i][1] = y
-                x = random.randrange(0, 550)
-                snow_list[i][0] = x
 
         pygame.display.flip()
         clock.tick(80)
@@ -155,12 +144,64 @@ class Tile(pygame.sprite.Sprite):
             tile_width * pos_x, tile_height * pos_y)
 
 
+class Monster(pygame.sprite.Sprite):
+    def __init__(self, pos_x, pos_y, axis):
+        super().__init__(tiles_group, all_monsters)
+        scale = (80, 70)
+        self.schet = 0
+        self.all_im = [pygame.transform.scale(load_image('monster\Wraith_01_Moving Forward_000.png'), scale),
+                       pygame.transform.scale(load_image('monster\Wraith_01_Moving Forward_001.png'), scale),
+                       pygame.transform.scale(load_image('monster\Wraith_01_Moving Forward_002.png'), scale),
+                       pygame.transform.scale(load_image('monster\Wraith_01_Moving Forward_003.png'), scale),
+                       pygame.transform.scale(load_image('monster\Wraith_01_Moving Forward_004.png'), scale),
+                       pygame.transform.scale(load_image('monster\Wraith_01_Moving Forward_005.png'), scale),
+                       pygame.transform.scale(load_image('monster\Wraith_01_Moving Forward_006.png'), scale),
+                       pygame.transform.scale(load_image('monster\Wraith_01_Moving Forward_007.png'), scale),
+                       pygame.transform.scale(load_image('monster\Wraith_01_Moving Forward_008.png'), scale),
+                       pygame.transform.scale(load_image('monster\Wraith_01_Moving Forward_009.png'), scale),
+                       pygame.transform.scale(load_image('monster\Wraith_01_Moving Forward_010.png'), scale),
+                       pygame.transform.scale(load_image('monster\Wraith_01_Moving Forward_011.png'), scale)]
+
+        self.ind_im = 0
+        self.axis = axis
+        self.napr = 0
+        self.image = self.all_im[self.ind_im]
+        self.coef = random.choice([-1, 1])
+        self.mask = pygame.mask.from_surface(self.image)
+        self.rect = self.image.get_rect().move(
+            tile_width * pos_x - 10, tile_height * pos_y - 7)
+
+    def update(self):
+        if self.axis == 'y':
+            self.rect.y += 2 * self.coef
+            if self.schet % 15 == 0:
+                self.ind_im = (self.ind_im + 1) % len(self.all_im)
+                if self.coef == -1:
+                    self.image = pygame.transform.flip(self.all_im[self.ind_im], 1, 0)
+
+                else:
+                    self.image = self.all_im[self.ind_im]
+
+        if self.axis == 'x':
+            self.rect.x += 2 * self.coef
+            if self.schet % 15 == 0:
+                self.ind_im = (self.ind_im + 1) % len(self.all_im)
+                if self.coef == -1:
+                    self.image = pygame.transform.flip(self.all_im[self.ind_im], 1, 0)
+
+                else:
+                    self.image = self.all_im[self.ind_im]
+
+
 class Player(pygame.sprite.Sprite):
-    def __init__(self, pos_x, pos_y):
-        super().__init__(player_group, all_sprites)
+    def __init__(self, pos_x, pos_y, control='k'):
+        super().__init__(player_group, all_players)
         self.image = player_image
+        self.control = control
         self.rect = self.image.get_rect().move(
             tile_width * pos_x + 15, tile_height * pos_y + 5)
+
+        self.mask = pygame.mask.from_surface(self.image)
 
 
 class Fruit(pygame.sprite.Sprite):
@@ -173,6 +214,7 @@ class Fruit(pygame.sprite.Sprite):
 
 def generate_level(level):
     new_player, x, y = None, None, None
+    monsters = 0
     for y in range(len(level)):
         for x in range(len(level[y])):
             if level[y][x] == '.':
@@ -184,6 +226,17 @@ def generate_level(level):
                 new_player = Player(x, y)
             elif level[y][x] == '@':
                 Tile('ice', x, y)
+            elif level[y][x] == '*':
+                Tile('empty', x, y)
+                if monsters == 2:
+                   Monster(x, y, 'x')
+
+                elif monsters == 1 or monsters == 3:
+                    Monster(x, y, 'y')
+
+                else:
+                    Monster(x, y, 'x')
+                monsters += 1
     # вернем игрока, а также размер поля в клетках
     return new_player, x, y
 
@@ -203,25 +256,17 @@ def main(lvl_num):
 
     fruits_flag = True
 
-
     while True:
         all_sprites.draw(screen)
         all_fruits.draw(screen)
         screen.blit(player_image, (player.rect.x, player.rect.y))
+        for monster in all_monsters:
+            screen.blit(monster.image, (monster.rect.x, monster.rect.y))
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 terminate()
         keys = pygame.key.get_pressed()
         if keys[pygame.K_LEFT]:
-            player.rect.x -= 5
-            for i in all_sprites:
-                if i.image != tile_images['empty'] and i.rect.colliderect(player.rect) and i.image != player_image:
-                    player.rect.x += 5
-            for i in all_fruits:
-                if i.rect.colliderect(player.rect):
-                    all_fruits.remove(i)
-
-        if keys[pygame.K_a]:
             player.rect.x -= 5
             for i in all_sprites:
                 if i.image != tile_images['empty'] and i.rect.colliderect(player.rect) and i.image != player_image:
@@ -239,25 +284,7 @@ def main(lvl_num):
                 if i.rect.colliderect(player.rect):
                     all_fruits.remove(i)
 
-        if keys[pygame.K_d]:
-            player.rect.x += 5
-            for i in all_sprites:
-                if i.image != tile_images['empty'] and i.rect.colliderect(player.rect) and i.image != player_image:
-                    player.rect.x -= 5
-            for i in all_fruits:
-                if i.rect.colliderect(player.rect):
-                    all_fruits.remove(i)
-
         if keys[pygame.K_UP]:
-            player.rect.y -= 5
-            for i in all_sprites:
-                if i.image != tile_images['empty'] and i.rect.colliderect(player.rect) and i.image != player_image:
-                    player.rect.y += 5
-            for i in all_fruits:
-                if i.rect.colliderect(player.rect):
-                    all_fruits.remove(i)
-
-        if keys[pygame.K_w]:
             player.rect.y -= 5
             for i in all_sprites:
                 if i.image != tile_images['empty'] and i.rect.colliderect(player.rect) and i.image != player_image:
@@ -275,21 +302,20 @@ def main(lvl_num):
                 if i.rect.colliderect(player.rect):
                     all_fruits.remove(i)
 
-        if keys[pygame.K_s]:
-            player.rect.y += 5
-            for i in all_sprites:
-                if i.image != tile_images['empty'] and i.rect.colliderect(player.rect) and i.image != player_image:
-                    player.rect.y -= 5
-            for i in all_fruits:
-                if i.rect.colliderect(player.rect):
-                    all_fruits.remove(i)
-
-
         if len(all_fruits) == 0 and fruits_flag:
             generate_fruits(load_level(f"lvl{lvl_num}_fruits2.txt"), random.choice(list(fruit_images.values())))
             fruits_flag = False
         if len(all_fruits) == 0 and not fruits_flag:
             terminate()
+
+        for monster in all_monsters:
+            monster.update()
+            for i in all_sprites:
+                if i.image != tile_images['empty'] and i.image != player_image and pygame.sprite.collide_mask(i, monster):
+                    monster.coef *= -1
+                if i.image == player_image and pygame.sprite.collide_mask(i, monster) and not keys[pygame.K_g]:
+                    terminate()
+
         pygame.display.flip()
         clock.tick(FPS)
 
@@ -303,20 +329,27 @@ FPS = 20
 clock = pygame.time.Clock()
 # основной персонаж
 player = None
+
 # группы спрайтов
 all_sprites = pygame.sprite.Group()
 tiles_group = pygame.sprite.Group()
 player_group = pygame.sprite.Group()
+
 start_screen()
+
 all_sprites = pygame.sprite.Group()
 tiles_group = pygame.sprite.Group()
 player_group = pygame.sprite.Group()
+
 level = third_screen()
+
 all_sprites = pygame.sprite.Group()
 tiles_group = pygame.sprite.Group()
 all_fruits = pygame.sprite.Group()
+all_monsters = pygame.sprite.Group()
+all_players = pygame.sprite.Group()
 player_group = pygame.sprite.Group()
-# все изображения для карты заносим в словарь
+# все изобра`жения для карты заносим в словарь
 tile_images = {
     'wall': load_image('box.png'),
     'empty': load_image('snow.png'),
